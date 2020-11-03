6e9951aafd55bfb1ac81cfca7d2a57fcf8fea30e
j-eric855 <71041220+j-eric855@users.noreply.github.com>
2020-11-03 12:51:14 +0100

    Update AUDI_LED.ino

--- AUDI_LED/AUDI_LED.ino
+++ AUDI_LED/AUDI_LED.ino
@@ -8,16 +8,10 @@
 */
 #include <OneButton.h>
 unsigned long zeitVorher = 0;
-unsigned long warteZeit = 100;
+unsigned long warteZeit = 1000;
+byte zaehler = 0;
 byte led[8] = {2, 3, 4, 5, 6, 7, 8, 9};
-byte ledZustand[6][8] = {
-  {1, 0, 0, 0, 0, 0, 0, 1},
-  {0, 1, 0, 0, 0, 0, 1, 0},
-  {0, 0, 1, 0, 0, 1, 0, 0},
-  {0, 0, 0, 1, 1, 0, 0, 0},
-  {0, 0, 1, 0, 0, 1, 0, 0},
-  {0, 1, 0, 0, 0, 0, 1, 0},
-};
+
 OneButton taster(10, true);
 bool merkerPower = false;
 
@@ -34,29 +28,27 @@ void setup()
 
 void loop()
 {
-  unsigned long zeitJetzt = millis();
+  
   taster.tick();
   delay(10);
   Serial.println(merkerPower);
   if  (merkerPower == true)
   {
-
-    for (byte i = 0; i < 6; i++)                  //Zeilen
+    unsigned long zeitJetzt = millis();
+    if ((zeitJetzt - zeitVorher) > warteZeit)
     {
-      if (zeitJetzt - zeitVorher > warteZeit)    //Verzögerung von warteZeit
-      {
-
-        for (byte j = 0; j < 8; j++)              //Spalten
-        {
-          digitalWrite(led[j], ledZustand[i][j]);
-
-
-        }
-        zeitJetzt = zeitVorher;
-      }
-
+      digitalWrite( led[zaehler], LOW);
+      digitalWrite( led[zaehler +1], HIGH);
+      zaehler ++;
     }
+    
+    if (zaehler == 8)
+    {
+      zaehler = 0;
+    }
+    zeitVorher = zeitJetzt;
   }
+
 
 
 }
